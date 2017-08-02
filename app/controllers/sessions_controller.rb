ebd66464f2a9d11661623ab4eed4a60f6169f098
Michael Orlando <michael.john.orlando@gmail.com>
2017-08-01 19:28:36 -0700

    fixed a controller... def didnt have end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -11,9 +12,12 @@ class SessionsController < ApplicationController
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
+    end
   end
+
   def destroy
     log_out
     redirect_to root_url
   end
+
 end
