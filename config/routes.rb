0f4e8b063a8cbc817f1469595ffdcead791f974e
Michael Orlando <michael.john.orlando@gmail.com>
2017-08-01 17:24:39 -0700

    user sign-up works

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
+  post '/signup',  to: 'users#create'
+  
+  resources :users
+
   root 'static_pages#home'
 end
