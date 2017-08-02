3189180dcb419fd4dd3962bbbc920b6622b97bce
Michael Orlando <michael.john.orlando@gmail.com>
2017-08-02 10:54:59 -0700

    better login with cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,7 @@ module SessionsHelper
   # Logs out the current user.
   def log_out
     session.delete(:user_id)
+    forget(current_user)
     @current_user = nil
   end
 
@@ -19,5 +20,29 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[remember_tolken] = user.remember_tolken
+  end
+
+  #Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id:user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_tolken])
+        log_in user
+        @current_user = user
+      end
+    end
 
 end
